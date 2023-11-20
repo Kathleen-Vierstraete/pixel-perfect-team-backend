f9cbed76ee8d9c7fafabd688d7e5e019f4cc5ce7
Ben <benjamin.chuffart01@gmail.com>
2023-11-20 10:40:22 +0100

    fix: mess

--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -5,21 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Address;
 use App\Repository\AddressRepository;
 
-<<<<<<< HEAD
-class AddressController extends AbstractController
-{
-    #[Route('/address', name: 'app_address')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AddressController.php',
-        ]);
-=======
 #[Route('/api', name: 'api_')]
 class AddressController extends AbstractController
 {
@@ -29,7 +16,5 @@ class AddressController extends AbstractController
         $addresses = $addressRepository->findAll();
 
         return $this->json($addresses, 200, [], ['groups' => 'address:read']);
->>>>>>> 5327beda1ce5dd15e952a0b283a9aafd8be0f00e
     }
-
 }
