081ccb49e4bdd0720c3d4926b1d0bc8ef175d89e
Eric Sergueev <eric.sergueev@gmail.com>
2023-12-04 14:15:14 +0100

    feat : add route get product by category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 
 #[Route('/api/categories', name: 'api_category_')]
 class CategoryController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(CategoryRepository $categoryRepository ): JsonResponse
+    #[Route('', name: 'index', methods: "GET")]
+    public function index(CategoryRepository $categoryRepository): JsonResponse
     {
-        $categories= $categoryRepository->findAll();
+        $categories = $categoryRepository->findAll();
 
         return $this->json($categories, 200, [], ['groups' => 'category:crud']);
+    }
+
+    #[Route('/{id<\d+>}/products', name: 'product_by_category', methods: "GET")]
+    public function getProductByCategorie(Category $category, ProductRepository $productRepository): JsonResponse
+    {
+        $categories = $productRepository->findBy(["category" => $category]);
+
+        return $this->json($categories, 200, [], ['groups' => 'product:crud']);
     }
 }
