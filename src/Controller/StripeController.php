831b2543ed3a0d073c6bd4789ebe61d3a1a54d02
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-12-19 10:50:52 +0100

    feat: adding more properties for stripe in api method

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -20,17 +20,43 @@ class StripeController extends AbstractController
         Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
 
         // Retrieve necessary data from the request (e.g., items, amount, currency)
-        // $requestData = json_decode($request->getContent(), true);
-        // // TODO : get properties
-        // $items = $requestData['items'];
+        $requestData = json_decode($request->getContent(), true);
+        // TODO : get properties
+        $items = $requestData['items'];
+
+        function calculateOrderAmount(array $items): int {
+            // Replace this constant with a calculation of the order's amount
+            // Calculate the order total on the server to prevent
+            // people from directly manipulating the amount on the client
+
+            $totalAmount = 0;
+
+            foreach ($items as $item) {
+                $totalAmount += $item['amount'];
+            }
+
+            return $totalAmount;
+        }
+
 
         try {
+            // retrieve JSON from POST body
+            $jsonStr = file_get_contents('php://input');
+            $jsonObj = json_decode($jsonStr, true);
+
+            // Convert the JSON object to an array
+            $itemsArray = $jsonObj['items'];
+
+            $totalAmount = calculateOrderAmount($itemsArray);
+
+
             // Create a PaymentIntent
             $paymentIntent = PaymentIntent::create([
-                'amount' => 1000, // Replace with the amount in cents
-                'currency' => 'usd', // Replace with your desired currency
-                'description' => 'Payment for items', // Replace with description
-                // Add any additional parameters as needed
+                'amount' => $totalAmount,
+                // 'amount' => 1000, // Replace with the amount in cents
+                'currency' => 'eur', // Replace with your desired currency
+                // 'description' => 'Payment for items', // Replace with description
+                // // Add any additional parameters as needed
             ]);
 
             // Return the client secret in the response
